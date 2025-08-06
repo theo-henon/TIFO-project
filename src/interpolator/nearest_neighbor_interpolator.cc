63da44c0c72f5e7ff8862d09316472e59688863f
Théo Hénon <theo.henon@epita.fr>
2025-08-06 11:03:23 +0200

    refactor(NearestNeighborInterpolator): follow some clang tidy suggestions

--- src/interpolator/nearest_neighbor_interpolator.cc
+++ src/interpolator/nearest_neighbor_interpolator.cc
@@ -6,15 +6,16 @@ namespace tifo
 {
     std::optional<uint8_t> NearestNeighborInterpolator::interpolate(const Image& img, const Vector3f& pixel_pos) const
     {
-        uint32_t pixel_idx = std::round(pixel_pos.x);
-        uint32_t pixel_idy = std::round(pixel_pos.y);
-        uint32_t pixel_idz = std::round(pixel_pos.z);
+        const auto pixel_idx = static_cast<uint32_t>(std::round(pixel_pos.x));
+        const auto pixel_idy = static_cast<uint32_t>(std::round(pixel_pos.y));
+        const auto pixel_idz = static_cast<uint32_t>(std::round(pixel_pos.z));
 
         try
         {
-            return std::make_optional(img.get_pixel(pixel_idx, pixel_idy, pixel_idz));
+            const uint8_t color = img.get_pixel(pixel_idx, pixel_idy, pixel_idz);
+            return std::make_optional(color);
         }
-        catch (const std::out_of_range& oor_ex)
+        catch (const std::out_of_range&)
         {
             return std::nullopt;
         }
